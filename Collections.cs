41d5c3857279e82c5c320c3b824b1d246ea72148
Gimel Contillo <61m37.17@gmail.com>
2018-06-17 00:38:55 +0800

    initial commit

---
+++ Collections.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListShuffler
+{
+    public class Collections
+    {
+        private static Random rnd;
+
+        static Collections()
+        {
+            rnd = new Random();
+        }
+
+        public List<T> Shuffle<T>(List<T> l)
+        {
+            List<T> newList = new List<T>();
+            int count = l.Count;
+            List<int> indes = new List<int>();
+            indes.Add(rnd.Next(0, count));
+
+            if (count > 0)
+            {
+                int j = 0;
+                while (j != l.Count - 1)
+                {
+                    bool isIndexExist = false;
+                    int generated_index = rnd.Next(0, count);
+
+                    for (int i = 0; i < indes.Count; i++)
+                    {
+                        if (indes.ElementAt(i) == generated_index)
+                        {
+                            isIndexExist = true;
+                            break;
+                        }
+                    }
+
+                    if (!isIndexExist)
+                    {
+                        indes.Add(generated_index);
+                        j++;
+                    }
+                }
+            }
+
+            foreach (int z in indes)
+            {
+                newList.Add(l.ElementAt(z));
+
+            }
+
+
+            return newList;
+        }
+
+    }
+}
